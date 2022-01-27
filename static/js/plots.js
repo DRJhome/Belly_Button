0aa88df95344c7fc324c3e9f1803cba46b459ebb
DRJhome <91449005+DRJhome@users.noreply.github.com>
2022-01-26 18:17:18 -0800

    UpdateIndex

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,7 +1,7 @@
 function init() {
     var selector = d3.select("#selDataset");
 
-    d3.json("samples.json").then((data) => {
+    d3.json("./static/js/samples.json").then((data) => {
         console.log(data);
         var sampleNames = data.names;
         sampleNames.forEach((sample) => {
@@ -19,12 +19,12 @@ init();
 function optionChanged(newSample) {
     console.log(newSample);
     buildMetadata(newSample);
-    buildCharts(newSample);
+    //buildCharts(newSample);
 }
 
 // buildMetadata function called in optionChanged
 function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
+    d3.json("./static/js/samples.json").then((data) => {
         var metadata = data.metadata;
         var resultsArray = metadata.filter(sampleObj => sampleObj.id == sample);
         var result = resultsArray[0]; //Because results are returned like an array =[0]
